1f140785cebee4887695e2fbec396f5d41214d51
Blair Frandeen <blairfrandeen@gmail.com>
2023-04-16 15:31:28 -0700

    add cargo installs

---
+++ cargoinstall.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Cargo binaries to install
+cargo install bat
+cargo install cargo-info
+cargo install exa
+cargo install flamegraph
+cargo install git-delta
+cargo install mdbook
+cargo install ripgrep
+cargo install rustlings
+cargo install tokei
+
