2549a8de2d2ffb84c5a2293a187bf9748cb6ad71
Blair Frandeen <Blair.S.Frandeen@jpl.nasa.gov>
2023-05-23 11:06:04 -0700

    add powershell profile

--- install.bat
+++ install.bat
@@ -6,5 +6,8 @@ mklink /J "C:\Users\%username%\My Drive\Notes\.obsidian" "C:\Users\%username%\do
 obsidian"
 mklink /J "C:\Users\%username%\AppData\Roaming\espanso" "C:\Users\%username%\dotfiles\espanso"
 mklink "C:\Users\%username%\AppData\Roaming\Code\User\settings.json" "C:\Users\%username%\dotfiles\vscode"
+mklink /J "C:\Users\%username%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" "C:\Users\%username%\dotfiles\profile.ps1"
+mklink /J "C:\Users\%username%\Documents\WindowsPowerShell\profile.ps1" "C:\Users\%username%\dotfiles\profile.ps1"
+
 echo "Getting submodules..."
 git submodule update --init --remote
---
+++ profile.ps1
@@ -0,0 +1,28 @@
+# PowerShell Profile
+# Run `Set-ExecutionPolicy Bypass` from an admin shell if not working
+
+# Aliases
+#########
+function GitStatus {git status}
+function GitPull {git pull}
+function GitPush {git push}
+function PythonActivateVenv {venv\scripts\activate}
+Set-Alias gs GitStatus
+Set-Alias -Name gp -Value GitPull -Force -Option Allscope
+Set-Alias -Name cat -Value bat -Force -Option Allscope
+Set-Alias gpu GitPush
+Set-Alias grep rg
+Set-Alias actenv PythonActivateVenv
+
+
+# Quality-Of-Life
+#################
+Set-PSReadLineOption -EditMode vi -BellStyle None
+Set-PSReadLineKeyHandler -Key Ctrl+p -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key Ctrl+n -Function HistorySearchForward
+
+# Fzf
+# Used The following commands from an admin shell to get this working:
+# choco install fzf
+# Install-Module -Name PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
