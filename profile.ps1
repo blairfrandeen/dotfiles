e6bbe1001ec1eca74d1cffc451624069e4aa723b
Blair Frandeen <Blair.S.Frandeen@jpl.nasa.gov>
2023-09-26 14:21:39 -0700

    add nice git log alias

--- profile.ps1
+++ profile.ps1
@@ -6,13 +6,16 @@
 function GitStatus {git status}
 function GitPull {git pull}
 function GitPush {git push}
+function GitLog {git log --pretty=format:'%C(yellow)%h%C(reset) %s %C(cyan)%cd%C(reset) %C(blue)%an%C(reset) %C(green)%d%C(reset)' --date=format:'%a %b %e, %Y' --graph}
 function PythonActivateVenv {venv\scripts\activate}
 Set-Alias gs GitStatus
+Set-Alias gl GitLog -Force -Option Allscope
 Set-Alias -Name gp -Value GitPull -Force -Option Allscope
 Set-Alias -Name cat -Value bat -Force -Option Allscope
 Set-Alias gpu GitPush
 Set-Alias grep rg
 Set-Alias actenv PythonActivateVenv
+Set-Alias vim nvim
 
 # Activate a pipx virtual environment based on the current directory
 function actx {
