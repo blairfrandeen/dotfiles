32ccdb963cadeb8c6ce3fac9370eb1bd471f7a35
Blair Frandeen <blairfrandeen@gmail.com>
2025-01-04 05:24:54 -0800

    add aliases for Python / UV

--- fish/config.fish
+++ fish/config.fish
@@ -32,3 +32,11 @@ abbr -a dots 'cd ~/dotfiles'
 set -x FLYCTL_INSTALL "/home/blair/.fly"
 set -x PATH "$FLYCTL_INSTALL/bin" $PATH
 
+# UV for python
+alias python3.8 'uv run --python=3.8 python3'
+alias python3.9 'uv run --python=3.9 python3'
+alias python3.10 'uv run --python=3.10 python3'
+alias python3.11 'uv run --python=3.11 python3'
+alias python3.12 'uv run --python=3.12 python3'
+alias python3.13 'uv run --python=3.13 python3'
+alias python3 python3.12
