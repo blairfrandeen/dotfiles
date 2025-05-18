616d2a559b24ac0872907587c0e0a2442b04df19
Blair Frandeen <blairfrandeen@gmail.com>
2025-05-18 16:04:09 -0700

    WIP: upgrade vim config to lua. Add error-lens and gitsigns

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -23,17 +23,125 @@ vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
 require("lazy").setup({
+{
+    "chikko80/error-lens.nvim",
+    event = "BufRead",
+    dependencies = {
+        "nvim-telescope/telescope.nvim"
+    },
+    opts = {
+        -- your options go here
+    },
+},
+    -- {
+    --     "folke/trouble.nvim",
+    --     opts = {}, -- for default options, refer to the configuration section for custom setup.
+    --     cmd = "Trouble",
+    --     keys = {
+    --         {
+    --             "<leader>xx",
+    --             "<cmd>Trouble diagnostics toggle<cr>",
+    --             desc = "Diagnostics (Trouble)",
+    --         },
+    --         {
+    --             "<leader>xX",
+    --             "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+    --             desc = "Buffer Diagnostics (Trouble)",
+    --         },
+    --         {
+    --             "<leader>cs",
+    --             "<cmd>Trouble symbols toggle focus=false<cr>",
+    --             desc = "Symbols (Trouble)",
+    --         },
+    --         {
+    --             "<leader>cl",
+    --             "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+    --             desc = "LSP Definitions / references / ... (Trouble)",
+    --         },
+    --         {
+    --             "<leader>xL",
+    --             "<cmd>Trouble loclist toggle<cr>",
+    --             desc = "Location List (Trouble)",
+    --         },
+    --         {
+    --             "<leader>xQ",
+    --             "<cmd>Trouble qflist toggle<cr>",
+    --             desc = "Quickfix List (Trouble)",
+    --         },
+    --     },
+    -- },
+    {
+        "lewis6991/gitsigns.nvim",
+        opts = {
+            on_attach = function(bufnr)
+                local gs = package.loaded.gitsigns
+
+                -- Standard preview
+                vim.keymap.set('n', '<leader>gp', gs.preview_hunk, { buffer = bufnr })
+
+                -- Preview with hunk navigation
+                vim.keymap.set('n', '<leader>gj', function()
+                    if vim.wo.diff then return ']c' end
+                    vim.schedule(function() gs.next_hunk() end)
+                    return '<Ignore>'
+                end, { expr = true, buffer = bufnr })
+
+                vim.keymap.set('n', '<leader>gk', function()
+                    if vim.wo.diff then return '[c' end
+                    vim.schedule(function() gs.prev_hunk() end)
+                    return '<Ignore>'
+                end, { expr = true, buffer = bufnr })
+
+                -- Toggle hunk preview in floating window with scroll support
+                vim.keymap.set('n', '<leader>gt', gs.toggle_current_line_blame, { buffer = bufnr })
+
+                -- Open file diff in a split (easier to scroll through all changes)
+                vim.keymap.set('n', '<leader>gd', gs.diffthis, { buffer = bufnr })
+            end,
+        }
+    },
+    -- {
+    --     "airblade/vim-gitgutter",
+    --     event = { "BufReadPre", "BufNewFile" },
+    --     config = function()
+    --         -- Set up GitGutter custom highlights
+    --         vim.api.nvim_set_hl(0, "GitGutterAdd", { fg = "#00FF00" })
+    --         vim.api.nvim_set_hl(0, "GitGutterChange", { fg = "#00FFFF" })
+    --         vim.api.nvim_set_hl(0, "GitGutterDelete", { fg = "#FF0000" })
+    --
+    --         -- Set up keybinding for preview hunk
+    --         vim.keymap.set('n', '<leader>gp', '<Plug>(GitGutterPreviewHunk)',
+    --             { silent = true, noremap = false })
+    --
+    --         -- Optional: Additional GitGutter settings
+    --         vim.g.gitgutter_sign_added = '+'
+    --         vim.g.gitgutter_sign_modified = '~'
+    --         vim.g.gitgutter_sign_removed = '-'
+    --         vim.g.gitgutter_sign_removed_first_line = '‾'
+    --         vim.g.gitgutter_sign_modified_removed = '≃'
+    --
+    --         -- Optional: Update time (in milliseconds) - lower value for more responsive updates
+    --         vim.opt.updatetime = 100
+    --
+    --         -- Optional: Additional keybindings you might want
+    --         vim.keymap.set('n', '<leader>gn', '<Plug>(GitGutterNextHunk)', { silent = true, noremap = false })
+    --         vim.keymap.set('n', '<leader>gN', '<Plug>(GitGutterPrevHunk)', { silent = true, noremap = false })
+    --         vim.keymap.set('n', '<leader>ga', '<Plug>(GitGutterStageHunk)',
+    --             { silent = true, noremap = false })
+    --         vim.keymap.set('n', '<leader>gu', '<Plug>(GitGutterUndoHunk)', { silent = true, noremap = false })
+    --     end,
+    -- },
     {
         "ibhagwan/fzf-lua",
         dependencies = { "nvim-tree/nvim-web-devicons" },
         opts = {},
         keys = {
             -- This is one way to lazy load the plugin when the keybinding is pressed
-            { "<c-p>",     "<cmd>FzfLua files<cr>",        mode = { "n", "i", "v" }, desc = "FzfLua Files" },
-            { "<c-t>",     "<cmd>FzfLua buffers<cr>",      mode = { "n", "i", "v" }, desc = "FzfLua Buffers" },
-            { "<leader>g", "<cmd>FzfLua git_status<cr>",   mode = "n",               desc = "FzfLua Git Status" },
-            { "F",         "<cmd>FzfLua lgrep_curbuf<cr>", mode = "n",               desc = "FzfLua grep current buffer" },
-            { "<leader>/", "<cmd>FzfLua grep<cr>",         mode = "n",               desc = "FzfLua Grep" },
+            { "<c-p>",      "<cmd>FzfLua files<cr>",        mode = { "n", "i", "v" }, desc = "FzfLua Files" },
+            { "<c-t>",      "<cmd>FzfLua buffers<cr>",      mode = { "n", "i", "v" }, desc = "FzfLua Buffers" },
+            { "<leader>gs", "<cmd>FzfLua git_status<cr>",   mode = "n",               desc = "FzfLua Git Status" },
+            { "F",          "<cmd>FzfLua lgrep_curbuf<cr>", mode = "n",               desc = "FzfLua grep current buffer" },
+            { "<leader>/",  "<cmd>FzfLua grep<cr>",         mode = "n",               desc = "FzfLua Grep" },
         },
         lazy = true,
     },
