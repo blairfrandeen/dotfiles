abafafcf87cb5a1992500007d0f3ce437fe350c2
Blair Frandeen <blairfrandeen@gmail.com>
2025-08-17 06:23:39 -0700

    add better llm command

--- fish/config.fish
+++ fish/config.fish
@@ -40,3 +40,8 @@ alias python3.11 'uv run --python=3.11 python3'
 alias python3.12 'uv run --python=3.12 python3'
 alias python3.13 'uv run --python=3.13 python3'
 alias python3 python3.12
+
+# LLM with terse replies, piped to bat
+function lc
+      llm -t c $argv | bat --language=markdown --style=plain
+end
