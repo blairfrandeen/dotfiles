8c4dbe76fd49aa5ecd7300e99ba934485aa1105f
Blair Frandeen <blairfrandeen@gmail.com>
2026-07-24 20:57:35 -0700

    Some pi config

--- fish/config.fish
+++ fish/config.fish
@@ -28,9 +28,15 @@ abbr -a srfish 'source ~/.config/fish/config.fish'
 
 abbr -a dots 'cd ~/dotfiles'
 
-# fly io
+# PATH Extensions
+# ---------------
+# fly.io
 set -x FLYCTL_INSTALL "/home/blair/.fly"
 set -x PATH "$FLYCTL_INSTALL/bin" $PATH
+# go
+set -x PATH "/usr/local/go/bin" $PATH
+# pi agent
+set -x PATH "/home/blair/agents" $PATH
 
 # UV for python
 alias python3.8 'uv run --python=3.8 python3'
@@ -39,6 +45,7 @@ alias python3.10 'uv run --python=3.10 python3'
 alias python3.11 'uv run --python=3.11 python3'
 alias python3.12 'uv run --python=3.12 python3'
 alias python3.13 'uv run --python=3.13 python3'
+alias python3.14 'uv run --python=3.14 python3'
 alias python3 python3.12
 
 # LLM with terse replies, piped to bat
