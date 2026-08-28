93a2bbb7d82250cc063581caea7ffe1c9b01250f
Blair Frandeen <blairfrandeen@gmail.com>
2026-08-27 20:41:41 -0700

    Swap caps escape script

---
+++ fish/swapcaps.fish
@@ -0,0 +1,12 @@
+#!/usr/bin/env fish
+
+set current (gsettings get org.gnome.desktop.input-sources xkb-options)
+
+if string match -q "*caps:swapescape*" -- $current
+    gsettings set org.gnome.desktop.input-sources xkb-options "[]"
+    echo "🖥️ Caps-Escape Swap: OFF"
+else
+    gsettings set org.gnome.desktop.input-sources xkb-options "['caps:swapescape']"
+    echo "💻 Caps-Escape Swap: ON"
+end
+    
