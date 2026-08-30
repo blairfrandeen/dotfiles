e9ae57e85ea41eb1272b5d7ce0276f334b6a1cd5
Blair Frandeen <blairfrandeen@gmail.com>
2026-08-30 09:39:04 -0700

    Additional cargo packages and tmux plugins

--- cargoinstall.sh
+++ cargoinstall.sh
@@ -4,10 +4,14 @@ cargo install bat
 cargo install cargo-info
 cargo install exa
 cargo install flamegraph
+cargo install forgejo-cli
 cargo install git-delta
 cargo install mdbook
+cargo install resvg
 cargo install ripgrep
 cargo install rustlings
 cargo install tokei
 cargo install ytop
+cargo install yazi
+cargo install fd-find
 cargo install starship --locked
